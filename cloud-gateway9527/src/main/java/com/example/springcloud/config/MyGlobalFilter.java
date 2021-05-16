7351b30ba9c50a7788f6fb9cd10881cd5bbf0baf
xiaoT <13003672533@163.com>
2021-05-16 22:05:29 +0800

    网关自定义全局过滤器

---
+++ cloud-gateway9527/src/main/java/com/example/springcloud/config/MyGlobalFilter.java
@@ -0,0 +1,28 @@
+package com.example.springcloud.config;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * 自定义全局过滤器
+ */
+@Component
+public class MyGlobalFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String username = exchange.getRequest().getQueryParams().getFirst("username");
+        if (username == null) {
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return response.setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+}
